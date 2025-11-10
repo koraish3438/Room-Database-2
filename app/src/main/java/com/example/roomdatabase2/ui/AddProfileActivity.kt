45ff7194c16cb2043488856a08080b1a36bb6ca2
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-10 12:11:33 +0600

    1st commit

--- app/src/main/java/com/example/roomdatabase2/ui/AddProfileActivity.kt
+++ app/src/main/java/com/example/roomdatabase2/ui/AddProfileActivity.kt
@@ -1,16 +1,17 @@
-package com.example.roomdatabase2
+package com.example.roomdatabase2.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.roomdatabase2.R
 
-class MainActivity : AppCompatActivity() {
+class AddProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
